5b95caaed7967ab8e237cfa6602d863a70323299
Oscar Andrés Ochoa Villegas <oochoa@cohan.org.co>
2023-04-04 08:03:54 -0500

    Add initial changes with basic use of vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar color="primary" dark app>
+        <span>{{ titulo }}</span>
     </v-app-bar>
-
+    
     <v-main>
-      <router-view/>
+      <v-container fluid fill-height>
+        <create-user></create-user>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  import CreateUser from './components/CreateUser.vue'
+  
+  export default {
+    name: 'App',
+    components: { CreateUser },
+    data: () => ({
+      titulo: "Demo App"
+    }),
+  };
 </script>
---
+++ src/components/CreateUser.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout
+    justify-center
+    align-center
+  >
+    <v-flex xs12 sm8 md6 lg5 xl4>
+      <v-card>
+        <v-toolbar>
+          <v-toolbar-title>
+            Sign Up
+          </v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-text-field label="mail"></v-text-field>
+          <v-text-field
+            label="password"
+            type="password"
+          ></v-text-field>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
